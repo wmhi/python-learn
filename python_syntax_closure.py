9c3992e69a4c69da18932be36742c36d2d38bc25
wm <wm18822827507@outlook.com>
2017-05-23 22:11:27 +0800

    python_syntax_closure

---
+++ python_syntax_closure.py
@@ -0,0 +1,28 @@
+# embed function
+def function_first(x):
+    print("first function be called")
+    # here define second function
+    def function_second(y):
+        print("second function be called")
+        return x * y
+    # here call second function
+    return function_second(x)
+
+print(function_first(4))
+# closure function
+def function_first(x):
+    print("first function be called")
+    # here define second function
+    def function_second(y):
+        print("second function be called")
+        return x * y
+    # here call second function
+    return function_second
+
+# tmp_function_second store environment of function_second
+tmp_function_second = function_first(4)
+
+print(tmp_function_second)
+# tmp_function_second continues to do what function_first is not done
+print(tmp_function_second(5))
+
