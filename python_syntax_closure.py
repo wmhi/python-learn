58936a9f9a3400233a2e7b1fd669e1dda83b17da
wm <wm18822827507@outlook.com>
2017-05-23 22:28:51 +0800

    commit failure

--- python_syntax_closure.py
+++ python_syntax_closure.py
@@ -1,23 +1,33 @@
 # embed function
 def function_first(x):
     print("first function be called")
+
     # here define second function
     def function_second(y):
         print("second function be called")
         return x * y
+
     # here call second function
     return function_second(x)
 
+
 print(function_first(4))
+
+
 # closure function
 def function_first(x):
-    print("first function be called")
+    test_variable = 10
+    print("first function be called， test_variable is ", test_variable)
+
     # here define second function
     def function_second(y):
-        print("second function be called")
+        nonlocal test_variable
+        test_variable += 1
+        print("second function be called, test_variable is ", test_variable)
         return x * y
-    # here call second function
+
     return function_second
+
 
 # tmp_function_second store environment of function_second
 tmp_function_second = function_first(4)
@@ -26,3 +36,4 @@ print(tmp_function_second)
 # tmp_function_second continues to do what function_first is not done
 print(tmp_function_second(5))
 
+# git hub are ok
