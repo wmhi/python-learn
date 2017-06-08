ff4338ee3543edd6ffa1bfd2104a941fff53d255
wm <wm18822827507@outlook.com>
2017-06-08 22:48:57 +0800

    with syntax

--- python_syntax_exception.py
+++ python_syntax_exception.py
@@ -3,9 +3,12 @@
 #   coding
 # except Exception[as reason]:
 #    error handle coding
+
+# with ... as ... syntax can auto close file,when the file is not be use  
 try:
-    fp = open("not_exist_file.txt", "r")
-    fp.close()
+    with open("not_exist_file.txt", "r") as fp:
+        for each_line in fp:
+            print(each_line)
 # The OSerror value is assigned to reason
 except OSError as reason:
     print("open file err:" + str(reason))
@@ -14,7 +17,16 @@ except TypeError as reason:
 except (TabError, TimeoutError) as reason:
     print("open file err:" + str(reason))
 finally:
-    print("open file err:")
+    print("err:")
+
+# "except is like a special "if", it can be used with the "else"
+try:
+    int("123")
+# The OSerror value is assigned to reason
+except ValueError as reason:
+    print("err:" + str(reason))
+else:
+    print("success")
 
 # i can use "raise" create a error
 # IE:raise ZeroDivisionError
